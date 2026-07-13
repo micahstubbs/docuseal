03b5f4e33b48c385a5d9679cdf205da8e5b6913f
@micahstubbs <git@micah.fyi>
2026-07-13 15:49:36 -0700

    Show invitation status and allow expired-invite resend on users list (docuseal-aqh)

--- app/controllers/users_send_reset_password_controller.rb
+++ app/controllers/users_send_reset_password_controller.rb
@@ -8,7 +8,7 @@ class UsersSendResetPasswordController < ApplicationController
   def update
     authorize!(:manage, @user)
 
-    if @user.reset_password_sent_at && @user.reset_password_sent_at > LIMIT_DURATION.ago
+    if rate_limited?
       redirect_back fallback_location: settings_users_path, notice: I18n.t('email_has_been_sent_already')
     else
       @user.send_reset_password_instructions
@@ -16,5 +16,18 @@ class UsersSendResetPasswordController < ApplicationController
       redirect_back fallback_location: settings_users_path,
                     notice: I18n.t('an_email_with_password_reset_instructions_has_been_sent')
     end
+  end
+
+  private
+
+  # The rate limit only applies while the last email is recent AND the invitation
+  # is still valid. Once the invitation token has expired (never accepted and older
+  # than Devise.reset_password_within), allow an immediate resend so admins can
+  # recover stuck invitations without waiting out the throttle.
+  def rate_limited?
+    return false if @user.reset_password_sent_at.blank?
+    return false if @user.invitation_status == :expired
+
+    @user.reset_password_sent_at > LIMIT_DURATION.ago
   end
 end
